16ef84b9f386b6452463af3843ca09e8c58dba31
ayush <ayushbite@gmail.com>
2022-11-02 10:10:46 +0530

    React Router Learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Home from './pages/Home'
+import About from "./pages/About";
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
 function App()
 {
     return(
-        <div>hello</div>
+       <>
+       <BrowserRouter>
+           <Link to={"/"}>Home</Link>
+           <Link to={"/about"}>About</Link>
+           <Switch>
+
+               <Route path="/" component={Home} exact={"/"} ></Route>
+           <Route path="/about" component={About}></Route>
+           </Switch>
+       </BrowserRouter>
+       </>
     );
 }
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+const Home = () =>{
+    return(
+        <h1>welome to home page</h1>
+    )
+}
+export default Home;
